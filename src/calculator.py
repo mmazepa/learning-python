3937110d92254bf38161c335d8d2f66f20c990c0
mmazepa <maksymilianmariusz@gmail.com>
2020-11-09 00:33:40 +0100

    Simple text-based user interface (TUI).

--- src/calculator.py
+++ src/calculator.py
@@ -14,10 +14,39 @@ def typeCorrectNumberLoop(index):
         number = isNumber(number)
     return number;
 
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+arithmetic_operators = ["+", "-", "*", "/"]
+
+def mainMenu():
+    sign = ""
+    while (not sign in arithmetic_operators):
+        sign = input("Enter arithmetic operator \"+\", \"-\", \"*\" or \"/\": ")
+        if sign == "+":
+            return str(num1) + " + " + str(num2) + " = " + str(add(num1, num2))
+            break
+        elif sign == "-":
+            return str(num1) + " - " + str(num2) + " = " + str(subtract(num1, num2))
+            break
+        elif sign == "*":
+            return str(num1) + " * " + str(num2) + " = " + str(multiply(num1, num2))
+            break
+        elif sign == "/":
+            return str(num1) + " / " + str(num2) + " = " + str(divide(num1, num2))
+            break
+        print("Please type an arithmetic operator.")
+
 num1 = typeCorrectNumberLoop(1)
 num2 = typeCorrectNumberLoop(2)
 
-print(str(num1) + " + " + str(num2) + " = " + str(num1 + num2))
-print(str(num1) + " - " + str(num2) + " = " + str(num1 - num2))
-print(str(num1) + " * " + str(num2) + " = " + str(num1 * num2))
-print(str(num1) + " / " + str(num2) + " = " + str(num1 / num2))
+print(mainMenu())
