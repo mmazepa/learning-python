bf9a901d5b7fc4e153d9a586f39e3ac82241acc2
mmazepa <maksymilianmariusz@gmail.com>
2020-11-09 03:16:38 +0100

    Upgrading the text-based user interface.

--- src/calculator.py
+++ src/calculator.py
@@ -1,3 +1,43 @@
+def header():
+    print("     ____      _            _       _             ")
+    print("    / ___|__ _| | ___ _   _| | __ _| |_ ___  _ __ ")
+    print("   | |   / _` | |/ __| | | | |/ _` | __/ _ \| '__|")
+    print("   | |__| (_| | | (__| |_| | | (_| | || (_) | |   ")
+    print("    \____\__,_|_|\___|\__,_|_|\__,_|\__\___/|_| \n")
+
+def framedText(text):
+    length = len(text)+2
+    print("   ╔", end="")
+    for x in range(0, length):
+        if x < length-1:
+            print("═", end="")
+        else:
+            print("═╗")
+    print("   ║ " + text + " ║")
+    print("   ╚", end="")
+    for x in range(0, length):
+        if x < length-1:
+            print("═", end="")
+        else:
+            print("═╝")
+
+def printText(text, framed):
+    if framed:
+        framedText(text)
+    else:
+        print(text)
+
+def textWithIndent(text, indent):
+    for x in range(0, indent):
+        print(" ", end="")
+    print(text)
+
+def log(type, content):
+    textWithIndent("[" + type + "]: " + content, 3)
+
+def newLine():
+    print("")
+
 def isNumber(number):
     try:
         return int(number)
@@ -5,12 +45,12 @@ def isNumber(number):
         try:
             return float(number)
         except ValueError:
-            print("Please type a number.")
+            log("INFO", "Please type a number.")
 
 def typeCorrectNumberLoop(index):
     number = ""
     while (not isinstance(number, int)) and (not isinstance(number, float)):
-        number = input("Enter number " + str(index) + ": ")
+        number = input("   Enter number " + str(index) + ": ")
         number = isNumber(number)
     return number;
 
@@ -26,12 +66,12 @@ def multiply(num1, num2):
 def divide(num1, num2):
     return num1 / num2
 
-def mainMenu():
+def mainMenu(num1, num2):
     sign = ""
     function = ""
     arithmetic_operators = ["+", "-", "*", "/"]
     while (not sign in arithmetic_operators):
-        sign = input("Enter arithmetic operator \"+\", \"-\", \"*\" or \"/\": ")
+        sign = input("   Enter arithmetic operator \"+\", \"-\", \"*\" or \"/\": ")
         if sign == "+":
             function = add
         elif sign == "-":
@@ -41,10 +81,14 @@ def mainMenu():
         elif sign == "/":
             function = divide
         else:
-            print("Please type an arithmetic operator.")
-    print(str(num1) + " " + sign + " " + str(num2) + " = " + str(function(num1, num2)))
+            log("INFO", "Please type an arithmetic operator.")
+    newLine()
+    textWithIndent("Your result is:", 3)
+    textWithIndent(str(num1) + " " + sign + " " + str(num2) + " = " + str(function(num1, num2)), 6)
 
-num1 = typeCorrectNumberLoop(1)
-num2 = typeCorrectNumberLoop(2)
+header()
+framedText("Welcome in the calculator with text-based user interface.")
+newLine()
 
-mainMenu()
+mainMenu(typeCorrectNumberLoop(1), typeCorrectNumberLoop(2))
+newLine()
